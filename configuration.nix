46bb2f14ee995778005946fdee417ac4b2ce5956
Jack W <jack-git@cetera.uk>
2023-03-16 22:50:11 -0400

    chore: :sparkles: things?

--- configuration.nix
+++ configuration.nix
@@ -1,1 +1,13 @@
-{}
+{
+  # Branding
+  system.nixos.label = "method";
+  boot.plymouth.theme = "breeze";
+  boot.plymouth.logo = ./media/etc-48.png;
+  isoImage.appendToMenuLabel = "";
+  isoImage.efiSplashImage = ./media/etc-splash.png;
+  # Enable flakes
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+  # systemd initrd (Faster boot!)
+  # boot.initrd.systemd.enable = true;
+  isoImage.squashfsCompression = "gzip -Xcompression-level 1"; # Fuck this man
+}
